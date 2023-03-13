2735cbf1a4bd7a3c73d07c1eff01d6d95c590e24
arranclan <127542373+arranclan@users.noreply.github.com>
2023-03-13 13:58:05 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,18 @@
 basic.forever(function () {
-    if (input.soundLevel() > 80) {
+    if (input.soundLevel() > 60) {
         music.setVolume(255)
-        music.playTone(988, music.beat(BeatFraction.Whole))
+        music.playTone(988, music.beat(BeatFraction.Double))
         music.stopAllSounds()
         basic.showIcon(IconNames.Confused)
-        basic.pause(1000)
-        for (let index = 0; index < 4; index++) {
-            servos.P1.setAngle(90)
-            servos.P1.setAngle(180)
-            servos.P1.setAngle(90)
-            servos.P1.setAngle(180)
-        }
-    } else if (input.soundLevel() > 40) {
-        basic.showIcon(IconNames.Happy)
-        basic.pause(1000)
+        basic.pause(5000)
+        servos.P1.setAngle(90)
+    } else if (input.soundLevel() > 5) {
+        basic.showIcon(IconNames.Asleep)
+        basic.pause(2000)
+        servos.P1.setAngle(0)
     } else {
-        basic.showIcon(IconNames.Heart)
-        basic.pause(1000)
+        basic.showIcon(IconNames.Happy)
+        basic.pause(2000)
+        servos.P1.setAngle(0)
     }
 })
